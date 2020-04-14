680617b3dfbe3caed09a00e26248344df59f165b
LongDuong <ldc.longnd@gmail.com>
2020-04-14 13:38:38 +0700

    first commit

---
+++ ch-01/02-variable/01.php
@@ -0,0 +1,14 @@
+<?php
+	$firsName = 'Long';
+	$lastName = 'Duong';
+	$fullName = $firsName . ' ' . $lastName;
+
+	echo $fullName;
+	var_dump($firsName);
+
+	echo '<br>';
+	$number = 12;
+	echo gettype($number);
+	
+	echo $number;
+?>
---
+++ ch-01/02-variable/02.php
@@ -0,0 +1,5 @@
+<?php
+	$number = 12;
+	echo gettype($number);;
+	echo $number;
+?>
